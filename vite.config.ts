d38b6953f893d3cf5a41c2c844aa63bf9fad78fa
Altair Studio <bakrin_as@icloud.com>
2026-06-25 13:23:15 +0300

    Set base path for Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -17,6 +17,7 @@ function figmaAssetResolver() {
 }
 
 export default defineConfig({
+  base: '/Prodecor33ru/',
   plugins: [
     figmaAssetResolver(),
     // The React and Tailwind plugins are both required for Make, even if
